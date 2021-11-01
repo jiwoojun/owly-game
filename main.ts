bc37e4ecb6a7ce3de7e80fdaa7440d1b2ca3ee15
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2021-11-01 20:54:00 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (owly.isHittingTile(CollisionDirection.Bottom)) {
+        owly.vy += -80
+    }
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    game.reset()
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    info.changeScoreBy(-1)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    info.changeScoreBy(1)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    if (controller.up.isPressed()) {
+        owly.y += -5
+    }
+})
+let owly: Sprite = null
+owly = sprites.create(img`
+    . . . . . . . f . . . . . . . . 
+    . . . . . f f 3 f f . . . . . . 
+    . . . . f 1 1 1 1 1 f . . . . . 
+    . . . f f 3 3 1 3 3 f f . . . . 
+    . . f 1 1 3 f 3 f 3 1 1 f . . . 
+    . . f 1 1 3 3 f 3 3 1 1 f . . . 
+    . . f 1 1 3 3 3 3 3 1 1 f . . . 
+    . . f 1 1 1 1 1 1 1 1 1 f . . . 
+    . . f 3 3 3 3 3 3 3 3 3 f . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . . . . . . . . f f f f . . 
+    . . . . . . . . . . f 1 1 f . . 
+    . . . . . . . . . . f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+tiles.setTilemap(tilemap`level1`)
+controller.moveSprite(owly, 180, 0)
+owly.ay = 100
+scene.cameraFollowSprite(owly)
+tiles.placeOnRandomTile(owly, assets.tile`myTile1`)
+tiles.coverAllTiles(assets.tile`myTile1`, assets.tile`myTile`)
+info.setScore(0)
